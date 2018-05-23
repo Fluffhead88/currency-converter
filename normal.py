effbb38eb3cfad889163727ca9ecf7ecf857fec6
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-23 14:35:26 -0400

    simple converter, not doing the math between currencies

--- normal.py
+++ normal.py
@@ -1,5 +1,8 @@
 
 
+
+
+
 class Money:
     def __init__(self, amount, sym):
         self.amount = amount
@@ -18,7 +21,30 @@ class Money:
         else:
             return self
 
+    def to_eur(self):
+        if self.amount != 0:
+            return Money(self.amount * 0.856046, "EUR")
+        else:
+            return self
 
-x = Money(2, "JPY")
+    def to_btc(self):
+        if self.amount != 0:
+            return Money(self.amount * 0.000132802, "BTC")
+        else:
+            return self
+
+t = Money(100.00, "USD") + Money(56.32, "EUR")
+print(t.to_usd().amount)
+print(t.to_usd().sym)
+
+"""x = Money(2, "JPY")
 print(x.to_jpy().amount)
-print(x.to_jpy().sym
+print(x.to_jpy().sym)
+
+y = Money(2, "EUR")
+print(y.to_eur().amount)
+print(y.to_eur().sym)
+
+z = Money(2, "BTC")
+print(z.to_btc().amount)
+print(z.to_btc().sym)"""
