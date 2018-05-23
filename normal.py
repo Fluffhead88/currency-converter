87da8eccb472c294652d63a35e4508e4a4c0f351
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-23 18:10:23 -0400

    normal mode working for all currency conversions between types

--- normal.py
+++ normal.py
@@ -1,8 +1,5 @@
 
 
-
-
-
 class Money:
     def __init__(self, amount, sym):
         self.amount = amount
@@ -10,47 +7,47 @@ class Money:
 
 
     def to_usd(self):
-        if self.amount != 0:
+        if self.sym == "USD":
             return Money(self.amount * 1, self.sym)
-        else:
-            return self
+        if self.sym == "JPY":
+            return Money(self.amount * 0.00909426, self.sym)
+        if self.sym == "EUR":
+            return Money(self.amount * 1.17024, self.sym)
+        if self.sym == "XBT":
+            return Money(self.amount * 7569.11, self.sym)
 
     def to_jpy(self):
-        if self.amount != 0:
-            return Money(self.amount * 110.084, self.sym)
-        else:
-            return self
+        if self.sym == "USD":
+            return Money(self.amount * 1, self.sym)
+        if self.sym == "JPY":
+            return Money(self.amount * 109.958, self.sym)
+        if self.sym == "EUR":
+            return Money(self.amount * 128.699, self.sym)
+        if self.sym == "XBT":
+            return Money(self.amount * 836610.69, self.sym)
 
     def to_eur(self):
-        if self.amount != 0:
-            return Money(self.amount * 0.854739, self.sym)
-        else:
-            return self
+        if self.sym == "USD":
+            return Money(self.amount * 0.854388, self.sym)
+        if self.sym == "JPY":
+            return Money(self.amount * 0.00777060, self.sym)
+        if self.sym == "EUR":
+            return Money(self.amount * 1, self.sym)
+        if self.sym == "XBT":
+            return Money(self.amount * 6500.82, self.sym)
 
     def to_xbt(self):
-        if self.amount != 0:
-            return Money(self.amount * 0.000133278, self.sym)
-        else:
-            return self
-
-    def to_symbol(self):
-        if to_usd():
-            self.sym = "USD"
-        if to_jpy():
-            self.sym = "JPY"
-        if to_eur():
-            self.sym = "EUR"
-        if to_xbt():
-            self.sym = "XBT"
+        if self.sym == "USD":
+            return Money(self.amount * 0.000131307, self.sym)
+        if self.sym == "JPY":
+            return Money(self.amount * 0.00000119429, self.sym)
+        if self.sym == "EUR":
+            return Money(self.amount * 0.000153687, self.sym)
+        if self.sym == "XBT":
+            return Money(self.amount * 1, self.sym)
 
 
-x = Money(1, "JPY")
-print(x.to_jpy().amount, x.to_jpy().sym)
 
 
-y = Money(1, "EUR")
-print(y.to_eur().amount, y.to_eur().sym)
-
-
-z = Money(1, "XBT")
-print(z.to_xbt().amount, z.to_xbt().sym)
+x = Money(1, "USD")
+print(x.to_xbt().amount, x.to_xbt().sym)
